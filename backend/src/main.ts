56451fbcad15e83ed6825c6e970c626efc6893b9
Noe Saul Nuñez <noe.ar18@hotmail.com>
2021-01-10 18:59:13 -0600

    fix: swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+  const options= new DocumentBuilder()
+  .setTitle("Fiscalia").setDescription("Fiscalias Crud").setVersion("1.0").build();
+  const documents=SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup("api",app, documents);
   await app.listen(3001);
 }
 bootstrap();
